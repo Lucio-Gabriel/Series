c9020bda1790925fbffab559e5bc9949709661a4
Lucio-Dev <luciogabrieldeazevedoo@gmail.com>
2025-07-24 09:03:13 -0300

    create repository and service container

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,12 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Episode;
-use App\Models\Season;
 use App\Models\Serie;
+use App\Repositories\EloquentSeriesRepository;
 use App\Repositories\SeriesRepository;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\View\View;
 
 class SeriesController extends Controller
